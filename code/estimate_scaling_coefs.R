7e7832dd57be4530039dcfc7eb90f058142e346b
Nick Marzolf <nmarzolf91@gmail.com>
2023-01-18 15:14:29 -0500

    update gitignore, fix ln issue in scaling coefs, make coef csv available

--- code/estimate_scaling_coefs.R
+++ code/estimate_scaling_coefs.R
@@ -6,19 +6,19 @@ estimate_scaling_coefs <- function(dir = 'data/derived/hydraulics') {
                 nrow = length(files))
   colnames(out) <- c('site', 'a', 'a_se', 'b', 'b_se', 'c', 'c_se', 'd', 'd_se', 'e', 'e_se',
                      'f', 'f_se','r2_width', 'r2_vel', 'r2_depth', 'prod_coefs', 'sum_coefs')
-
+  
   for(i in 1:length(files)) {
     
     dat <- readr::read_csv(files[i]) %>% 
-      filter(mean_velocity_ms > 0)
+      dplyr::filter(mean_velocity_ms > 0)
     
-    site <- str_split(files[i], '/')[[1]][4] %>% 
+    site <- stringr::str_split(files[i], '/')[[1]][4] %>% 
       substr(.,1,4)
     
     lm_depth <- lm(data = dat %>% 
                      filter(is.finite(log(mean_depth_m)),
                             is.finite(log(calcQ_m3s))
-                            ),
+                     ),
                    log(mean_depth_m) ~ log(calcQ_m3s))
     c <- as.numeric(summary(lm_depth)$coefficients[1])
     c_se <- as.numeric(summary(lm_depth)$coefficients[3])
@@ -27,8 +27,8 @@ estimate_scaling_coefs <- function(dir = 'data/derived/hydraulics') {
     r2_depth <- summary(lm_depth)$adj.r.squared
     
     lm_width <- lm(data = dat %>% 
-                     filter(is.finite(log(mean_width_m)),
-                            is.finite(log(calcQ_m3s))
+                     dplyr::filter(is.finite(log(mean_width_m)),
+                                   is.finite(log(calcQ_m3s))
                      ),
                    log(mean_width_m) ~ log(calcQ_m3s))
     a <- as.numeric(summary(lm_width)$coefficients[1])
@@ -38,8 +38,8 @@ estimate_scaling_coefs <- function(dir = 'data/derived/hydraulics') {
     r2_width <- summary(lm_width)$adj.r.squared
     
     lm_vel <- lm(data = dat %>% 
-                   filter(is.finite(log(mean_velocity_ms)),
-                          is.finite(log(calcQ_m3s))
+                   dplyr::filter(is.finite(log(mean_velocity_ms)),
+                                 is.finite(log(calcQ_m3s))
                    ),
                  log(mean_velocity_ms) ~ log(calcQ_m3s))
     e <- as.numeric(summary(lm_vel)$coefficients[1])
@@ -52,15 +52,15 @@ estimate_scaling_coefs <- function(dir = 'data/derived/hydraulics') {
     prod_coefs <- a*c*e
     
     out[i,1] <- site
-    out[i,2] <- a
+    out[i,2] <- exp(a)
     out[i,3] <- a_se
     out[i,4] <- b
     out[i,5] <- b_se
-    out[i,6] <- c
+    out[i,6] <- exp(c)
     out[i,7] <- c_se
     out[i,8] <- d
     out[i,9] <- d_se
-    out[i,10] <- e
+    out[i,10] <- exp(e)
     out[i,11] <- e_se
     out[i,12] <- f
     out[i,13] <- f_se
@@ -71,23 +71,23 @@ estimate_scaling_coefs <- function(dir = 'data/derived/hydraulics') {
     out[i,18] <- sum_coefs
     
     plot <- dat %>% 
-      select(-est_mean_width_m) %>% 
-      pivot_longer(cols = 2:4,
-                   names_to = 'meas',
-                   values_to = 'val') %>% 
-      ggplot(.,
-             aes(x = log(calcQ_m3s),
-                 y = log(val)))+
-      geom_point()+
-      geom_smooth(method = 'lm')+
+      dplyr::select(-est_mean_width_m) %>% 
+      tidyr::pivot_longer(cols = 2:4,
+                          names_to = 'meas',
+                          values_to = 'val') %>% 
+      ggplot2::ggplot(.,
+                      ggplot2::aes(x = log(calcQ_m3s),
+                                   y = log(val)))+
+      ggplot2::geom_point()+
+      ggplot2::geom_smooth(method = 'lm')+
       # scale_x_log10()+
       # scale_y_log10()+
-      facet_grid(meas ~ ., 
-                 switch = 'both')+
-      theme_bw()+
-      labs(x = 'Discharge (m3/s)',
-           y = 'Width (m), Velocity (m/s), Depth (m)')+
-      ggtitle(site)
+      ggplot2::facet_grid(meas ~ ., 
+                          switch = 'both')+
+      ggplot2::theme_bw()+
+      ggplot2::labs(x = 'Discharge (m3/s)',
+                    y = 'Width (m), Velocity (m/s), Depth (m)')+
+      ggplot2::ggtitle(site)
     
     fig_dir <- 'figures'
     if(!dir.exists(fig_dir))
@@ -97,9 +97,9 @@ estimate_scaling_coefs <- function(dir = 'data/derived/hydraulics') {
     if(!dir.exists(fig_save_dir))
       dir.create(fig_save_dir)
     
-    ggsave(plot = plot,
-           glue('figures/hydraulic_scaling/{site}_hydraulicScaling.png'),
-           height = 6, width = 4)
+    ggplot2::ggsave(plot = plot,
+                    glue::glue('figures/hydraulic_scaling/{site}_hydraulicScaling.png'),
+                    height = 6, width = 4)
     
   } # end for loop
   
